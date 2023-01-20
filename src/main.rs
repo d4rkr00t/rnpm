35c6290698544ea3bebb70328acbaab9f3c8db2f
Stanislav Sysoev <d4rkr00t@gmail.com>
2023-01-20 20:25:31 +1100

    feat: very mvp impl of fetching npm packages defined in package-lock.json

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,67 @@
+use flate2::read::GzDecoder;
+use rayon::prelude::*;
+use tar::Archive;
+
+use std::fs::File;
+use std::io::Cursor;
+use std::path::{Path, PathBuf};
+
 mod parsers;
 
 fn main() {
     let contents = std::fs::read_to_string("example/package-lock.json").unwrap();
-    parsers::package_lock::parse(&contents);
+    let dg = parsers::package_lock::parse(&contents);
+
+    dg.par_iter().for_each(|(dest, id, pkg)| {
+        fetch_artifact(pkg.resolved.as_ref().unwrap(), &id, &dest);
+    });
+}
+
+fn fetch_artifact(req_url: &str, name: &str, dest: &str) {
+    println!("{}", name);
+
+    let dir_path = "/Users/ssysoev/Development/rnpm/example/artifacts/".to_string();
+    let tmp_path = "/Users/ssysoev/Development/rnpm/example/artifacts/tmp".to_string();
+    std::fs::create_dir_all(&dir_path).unwrap();
+    std::fs::create_dir_all(&tmp_path).unwrap();
+
+    let clean_name = name.replace("/", "__");
+    let artifact_path = format!("{}/{}.tgz", dir_path, clean_name);
+    let path = Path::new(&artifact_path);
+
+    let artifact_tmp_path = format!("{}/{}.tgz", tmp_path, clean_name);
+    let tmp_path = Path::new(&artifact_tmp_path);
+
+    if !path.exists() {
+        let mut file = File::create(path).unwrap();
+        println!("Downloading {}", req_url);
+        let body = reqwest::blocking::get(req_url).unwrap();
+        let mut content = Cursor::new(body.bytes().unwrap());
+        std::io::copy(&mut content, &mut file).unwrap();
+    }
+    let mut src = File::open(path).unwrap();
+    let mut tmp = File::create(tmp_path).unwrap();
+    std::io::copy(&mut src, &mut tmp).unwrap();
+
+    let tgz_file = File::open(tmp_path).unwrap();
+    let tar = GzDecoder::new(tgz_file);
+    let mut ar = Archive::new(tar);
+    let dest_path = format!("{}{}", dir_path, dest);
+    std::fs::create_dir_all(&dest_path).unwrap();
+    ar.entries().unwrap().for_each(|entry| {
+        let mut e = entry.unwrap();
+        let raw_path = e.path().unwrap();
+        let path_buf = raw_path.to_path_buf().to_owned();
+        let mut components = path_buf.components();
+        components.next();
+        let path = components.as_path().to_path_buf();
+        let dest = format!("{}/{}", dest_path, path.to_str().unwrap());
+        let mut p = PathBuf::from(dest.clone());
+        p.pop();
+        std::fs::create_dir_all(&p.as_path()).unwrap();
+
+        e.unpack(dest).unwrap();
+    });
 }
 
 // https://pnpm.io/symlinked-node-modules-structure
--- src/parsers/package_lock.rs
+++ src/parsers/package_lock.rs
@@ -1,26 +1,69 @@
 use serde::Deserialize;
-use std::collections::HashMap;
+use std::collections::{BTreeMap, HashMap};
 
 // https://docs.npmjs.com/cli/v9/configuring-npm/package-lock-json#file-format
 #[derive(Debug, Deserialize)]
 struct PackageLock {
-    version: String,
-    #[serde(rename(deserialize = "lockfileVersion"))]
-    lockfile_version: u32,
-    packages: HashMap<String, PackageLockPackage>,
+    // version: String,
+    // #[serde(rename(deserialize = "lockfileVersion"))]
+    // lockfile_version: u32,
+    packages: BTreeMap<String, PackageLockPackage>,
 }
 
-#[derive(Debug, Deserialize)]
-struct PackageLockPackage {
-    version: String,
-    resolved: Option<String>,
-    integrity: Option<String>,
-    bin: Option<HashMap<String, String>>,
+#[derive(Debug, Deserialize, Clone)]
+pub struct PackageLockPackage {
+    pub version: String,
+    pub resolved: Option<String>,
+    pub integrity: Option<String>,
+    pub bin: Option<HashMap<String, String>>,
     #[serde(rename(deserialize = "hasInstallScript"))]
-    has_install_script: Option<bool>,
+    pub has_install_script: Option<bool>,
 }
 
-pub fn parse(file_content: &str) {
+// {
+//   name_to_id: {
+//      @babel/code-frame@7.18.6: 0,
+//      @babel/code-generator@7.20.7: 1,
+//      ...
+//      @rollup/plugin-sucrase@2.0.2: 10
+//      estree-walker@2.0.2: 11
+//   },
+//   packages: [
+//      {@babel/code-frame@7.18.6, top_level},
+//      {@babel/code-generator@7.18.6, top_level},
+//      ...
+//      {@rollup/plugin-surcase@2.0.2, top_level},
+//      {estree-walker@2.0.2},
+//   ],
+//   dependencies:[
+//      [],
+//      []
+//      ...
+//      [11],
+//      []
+//   ]
+// }
+//
+// [(dest_path, package)]
+
+pub type PackagesVec = Vec<(String, String, PackageLockPackage)>;
+
+pub fn parse(file_content: &str) -> PackagesVec {
     let package_lock: PackageLock = serde_json::from_str(file_content).unwrap();
-    println!("{:#?}", package_lock);
+    let mut packages_vec: PackagesVec = Vec::new();
+
+    for (key, value) in package_lock.packages {
+        if key.len() == 0 {
+            continue;
+        }
+
+        let mut names: Vec<&str> = key["node_modules/".len()..]
+            .split("/node_modules/")
+            .collect();
+        let name = names.pop().unwrap().replace("@", "__").replace("/", "__");
+        let id = format!("{}__{}", name, value.version);
+        packages_vec.push((key, id, value));
+    }
+
+    return packages_vec;
 }
